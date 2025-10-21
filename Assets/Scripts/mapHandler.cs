cb7fe326f1c0044d99612c34b26a7c92409d8484
Gabriel Lizsica <gabor.lizsica@gmail.com>
2025-10-21 18:57:59 +0200

    Functions to create waypoints, storing them in a list, waypoint placeholder tile

--- Assets/Scripts/mapHandler.cs
+++ Assets/Scripts/mapHandler.cs
@@ -1,14 +1,20 @@
+using System.Collections.Generic;
+using System.Numerics;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class mapHandler : MonoBehaviour
 {
     [SerializeField] private Vector2Int mapSize;
+    [SerializeField] private int waypointCount;
     [SerializeField] private Tilemap tilemapGround;
     [SerializeField] private Tile tileEmpty;
     [SerializeField] private Tile tileBase;
     [SerializeField] private Tile tileEnemy;
+    [SerializeField] private Tile tileWaypoint;
+    
     
     public Vector2Int MapSize
     {
@@ -18,6 +24,8 @@ public class mapHandler : MonoBehaviour
     private Vector2Int enemyTilePos;
     private mapQuarters baseQuarter;
     private mapEdges enemyEdge;
+
+    private List<Vector2Int> waypoints = new List<Vector2Int>();
     
     private enum mapQuarters
     {
@@ -47,19 +55,26 @@ public class mapHandler : MonoBehaviour
             }
         }
         
-        baseTilePos = selectBaseTile();
+        selectBaseTile();
         baseQuarter = getMapQuarter(baseTilePos);
-        enemyTilePos = selectEnemyTile();
+        selectEnemyTile();
 
         tilemapGround.SetTile(new Vector3Int(baseTilePos.x, baseTilePos.y, 0), tileBase);
         tilemapGround.SetTile(new Vector3Int(enemyTilePos.x, enemyTilePos.y, 0), tileEnemy);
         Debug.Log("Base quarter: " + baseQuarter);
+
+        createPath();
+        
+        for (int i = 0; i < waypointCount; i++)
+        {
+            tilemapGround.SetTile(new Vector3Int(waypoints[i].x, waypoints[i].y, 0), tileWaypoint);
+        }
     }
     
     private mapQuarters getMapQuarter(Vector2Int tileChoords)
     {
         mapQuarters quarter = mapQuarters.DEFAULT;
-        Vector2 adjustedChoords = new Vector2(tileChoords.x + 1, tileChoords.y + 1);
+        Vector2Int adjustedChoords = new Vector2Int(tileChoords.x + 1, tileChoords.y + 1);
         
         if (adjustedChoords.x > mapSize.x / 2 && adjustedChoords.y > mapSize.y / 2)
         {
@@ -81,21 +96,16 @@ public class mapHandler : MonoBehaviour
         return quarter;
     }
     
-    private Vector2Int selectBaseTile()
+    private void selectBaseTile()
     {
-        Vector2Int _baseTilePos;
-        
         int xPos = UnityEngine.Random.Range(0, mapSize.x);
         int yPos = UnityEngine.Random.Range(0, mapSize.y);
         
-        _baseTilePos = new Vector2Int(xPos, yPos);
-        
-        return _baseTilePos;
+        baseTilePos = new Vector2Int(xPos, yPos);
     }
     
-    private Vector2Int selectEnemyTile()
+    private void selectEnemyTile()
     {
-        Vector2Int _enemyTilePos = new Vector2Int();
         int selectSide = UnityEngine.Random.Range(0, 2);
         
         switch (baseQuarter)
@@ -104,12 +114,12 @@ public class mapHandler : MonoBehaviour
                 if (selectSide == 1)
                 {
                     enemyEdge = mapEdges.BOTTOM;
-                    _enemyTilePos = new Vector2Int(UnityEngine.Random.Range(0, mapSize.x / 2), 0);
+                    enemyTilePos = new Vector2Int(UnityEngine.Random.Range(0, mapSize.x / 2), 0);
                 }
                 else
                 {
                     enemyEdge = mapEdges.LEFT;
-                    _enemyTilePos = new Vector2Int(0, UnityEngine.Random.Range(0, mapSize.y / 2));
+                    enemyTilePos = new Vector2Int(0, UnityEngine.Random.Range(0, mapSize.y / 2));
                 }
                 
                 break;
@@ -117,12 +127,12 @@ public class mapHandler : MonoBehaviour
                 if (selectSide == 1)
                 {
                     enemyEdge = mapEdges.BOTTOM;
-                    _enemyTilePos = new Vector2Int(UnityEngine.Random.Range(mapSize.x / 2, mapSize.x), 0);
+                    enemyTilePos = new Vector2Int(UnityEngine.Random.Range(mapSize.x / 2, mapSize.x), 0);
                 }
                 else
                 {
                     enemyEdge = mapEdges.RIGHT;
-                    _enemyTilePos = new Vector2Int(mapSize.x - 1, UnityEngine.Random.Range(0, mapSize.y / 2));
+                    enemyTilePos = new Vector2Int(mapSize.x - 1, UnityEngine.Random.Range(0, mapSize.y / 2));
                 }
                 
                 break;
@@ -130,12 +140,12 @@ public class mapHandler : MonoBehaviour
                 if (selectSide == 1)
                 {
                     enemyEdge = mapEdges.TOP;
-                    _enemyTilePos = new Vector2Int(UnityEngine.Random.Range(mapSize.x / 2, mapSize.x), mapSize.y - 1);
+                    enemyTilePos = new Vector2Int(UnityEngine.Random.Range(mapSize.x / 2, mapSize.x), mapSize.y - 1);
                 }
                 else
                 {
                     enemyEdge = mapEdges.RIGHT;
-                    _enemyTilePos = new Vector2Int(mapSize.x - 1, UnityEngine.Random.Range(mapSize.y / 2, mapSize.y));
+                    enemyTilePos = new Vector2Int(mapSize.x - 1, UnityEngine.Random.Range(mapSize.y / 2, mapSize.y));
                 }
                 
                 break;
@@ -143,19 +153,61 @@ public class mapHandler : MonoBehaviour
                 if (selectSide == 1)
                 {
                     enemyEdge = mapEdges.TOP;
-                    _enemyTilePos = new Vector2Int(UnityEngine.Random.Range(0, mapSize.x / 2), mapSize.y - 1);
+                    enemyTilePos = new Vector2Int(UnityEngine.Random.Range(0, mapSize.x / 2), mapSize.y - 1);
                 }
                 else
                 {
                     enemyEdge = mapEdges.LEFT;
-                    _enemyTilePos = new Vector2Int(0, UnityEngine.Random.Range(mapSize.y / 2, mapSize.y));
+                    enemyTilePos = new Vector2Int(0, UnityEngine.Random.Range(mapSize.y / 2, mapSize.y));
                 }
                 
                 break;
             default:
                 break;
         }
+    }
+    
+    private void createPath()
+    {
+        float mainDistance = Vector2Int.Distance(baseTilePos, enemyTilePos);
+        float lastDistance = mainDistance;
 
-        return _enemyTilePos;
+        for (int i = 0; i < waypointCount; i++)
+        {
+            waypoints.Add(createWaypoint(mainDistance, ref lastDistance));
+            Debug.Log("Next waypoint: " + waypoints[i].x + ", " + waypoints[i].y);
+        }
+    }
+    
+    private Vector2Int createWaypoint(float mainDistance, ref float lastDistance)
+    {
+        Vector2Int nextWaypoint = new Vector2Int();
+        float baseDistance, enemyDistance;
+        bool validPoint;
+
+        do
+        {
+            nextWaypoint = new Vector2Int(UnityEngine.Random.Range(0, mapSize.x), UnityEngine.Random.Range(0, mapSize.y));
+
+            baseDistance = Vector2Int.Distance(baseTilePos, nextWaypoint);
+            enemyDistance = Vector2Int.Distance(enemyTilePos, nextWaypoint);
+            
+            if (baseDistance < mainDistance && enemyDistance < mainDistance)
+            {
+                lastDistance = Vector2Int.Distance(baseTilePos, nextWaypoint);
+                validPoint = true;
+            }
+            else
+            {
+                validPoint = false;
+            }
+        } while (!validPoint);
+
+        return nextWaypoint;
+    }
+    
+    private void orderWaypoints()
+    {
+        
     }
 }
