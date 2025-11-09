ae139c72956b6ab7ac5f7a4c9399b4d6afcdfc3c
Gabriel Lizsica <gabor.lizsica@gmail.com>
2025-11-09 16:08:36 +0100

    New path generation system (doesnt work properly yet)

--- Assets/Scripts/mapHandler.cs
+++ Assets/Scripts/mapHandler.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -17,8 +18,8 @@ public class mapHandler : MonoBehaviour
     [SerializeField] private Tile tileWaypoint;
 
     [Header("Path Settings")]
-    [SerializeField] private Vector2Int startPos;
-    [SerializeField] private Vector2Int endPos;
+    [SerializeField] private Vector2Int pathStartPos;
+    [SerializeField] private Vector2Int pathEndPos;
     [SerializeField][Range(0, 1)] private float windingChance; 
     
     public Vector2Int MapSize
@@ -30,7 +31,7 @@ public class mapHandler : MonoBehaviour
     private mapQuarters baseQuarter;
     private mapEdges enemyEdge;
 
-    private List<Vector2Int> waypoints = new List<Vector2Int>();
+    [SerializeField] private List<Vector2Int> path = new List<Vector2Int>();
     
     private enum mapQuarters
     {
@@ -67,6 +68,8 @@ public class mapHandler : MonoBehaviour
         tilemapGround.SetTile(new Vector3Int(baseTilePos.x, baseTilePos.y, 0), tileBase);
         tilemapGround.SetTile(new Vector3Int(enemyTilePos.x, enemyTilePos.y, 0), tileEnemy);
         Debug.Log("Base quarter: " + baseQuarter);
+
+        generatePath();
     }
     
     private mapQuarters getMapQuarter(Vector2Int tileChoords)
@@ -163,5 +166,100 @@ public class mapHandler : MonoBehaviour
             default:
                 break;
         }
+    }
+    
+    private void generatePath()
+    {
+        path = createPath();
+        
+        foreach (var tile in path)
+        {
+            tilemapGround.SetTile(new Vector3Int(tile.x, tile.y, 0), tileWaypoint);
+        }
+    }
+    
+    private List<Vector2Int> createPath()
+    {
+        pathStartPos = baseTilePos;
+        pathEndPos = enemyTilePos;
+
+        List<Vector2Int> _path = new List<Vector2Int>();
+        _path.Add(Vector2Int.zero);
+        Vector2Int currentTile = pathStartPos;
+
+        Vector2Int[] dirs = { Vector2Int.up, Vector2Int.down, Vector2Int.right, Vector2Int.left };
+
+        int safety = 0;
+        
+        while (currentTile != pathEndPos && safety < 200)
+        {
+            safety++;
+            
+            Vector2Int preferredDir = Vector2Int.zero;
+            
+            if (Mathf.Abs(pathEndPos.x - currentTile.x) > Mathf.Abs(pathEndPos.y - currentTile.y))
+            {
+                preferredDir = (pathEndPos.x > currentTile.x) ? Vector2Int.right : Vector2Int.left;
+            }
+            else
+            {
+                preferredDir = (pathEndPos.y > currentTile.y) ? Vector2Int.up : Vector2Int.down;
+            }
+
+            Vector2Int moveDir = (Random.value < windingChance) ? dirs[Random.Range(0, dirs.Length)] : preferredDir;
+            Vector2Int nextTile = currentTile + moveDir;
+
+            if (_path.Contains(nextTile))
+            {
+                Debug.Log("Tile already occupied");
+                
+                for (int i = _path.Count - 1; i >= 0; i--)
+                {
+                    Debug.Log("Looking for last valid tile");
+                    
+                    if (freeNeighborCount(_path, i) <= 2)
+                    {
+                        _path.RemoveAt(i);
+                        Debug.Log("Deleted tile at: " + i);
+                    }
+                    else if (freeNeighborCount(_path, i) > 2)
+                    {
+                        currentTile = _path[i];
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                _path.Add(nextTile);
+                currentTile = nextTile;
+            }
+        }
+
+        return _path;
+    }
+    
+    private int freeNeighborCount(List<Vector2Int> tileList, int tileIndex)
+    {
+        int freeNeighbors = 0;
+        
+        if (!path.Contains(tileList[tileIndex] + Vector2Int.up))
+        {
+            freeNeighbors++;
+        }
+        if (!path.Contains(tileList[tileIndex] + Vector2Int.down))
+        {
+            freeNeighbors++;
+        }
+        if (!path.Contains(tileList[tileIndex] + Vector2Int.right))
+        {
+            freeNeighbors++;
+        }
+        if (!path.Contains(tileList[tileIndex] + Vector2Int.left))
+        {
+            freeNeighbors++;
+        }
+
+        return freeNeighbors;
     }
 }
